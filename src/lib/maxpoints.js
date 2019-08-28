d91b9486d32cecb58008fa8de095be5f6d281358
Bogdan Modzelewski <bogdan.modzelewski@procad.pl>
2019-08-28 14:25:26 +0200

    Support for new scoring system

--- src/lib/maxpoints.js
+++ src/lib/maxpoints.js
@@ -1,10 +1,10 @@
 export default function maxPoints(n, maxScorePerBlock) {
-  // first 7 blocks is arithmetic sequence
-  // a1=maxScorePerBlock, r=maxScorePerBlock, sum = (2 * a1 + (n-1) * r) / 2 * n
+  // first 14 blocks is arithmetic sequence
   // then for each block we get 8 * maxScorePerBlock
   return Math.floor(
-    ((2 * maxScorePerBlock + (Math.min(n, 7) - 1) * maxScorePerBlock) / 2) *
-      Math.min(n, 7) +
-      (n >= 8 ? (n - 7) * maxScorePerBlock * 8 : 0)
+      (n > 14 ? maxScorePerBlock * 8 * (n - 14) : 0) +
+      (n >= 7 ? maxScorePerBlock * 4 * (Math.min(n, 14) - 6) : 0) +
+      (n >= 3 ? maxScorePerBlock * 2 * (Math.min(n, 6) - 2) : 0) +
+      Math.min(n, 2) * maxScorePerBlock
   );
 }
